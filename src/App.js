0634377bd3957c59f4b20c72ad050a6779529f2f
ghost <kirk.douglas.richardson@gmail.com>
2017-09-13 19:39:21 -0600

    dynamic rendering

--- src/App.js
+++ src/App.js
@@ -2,67 +2,45 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'react-bootstrap';
-const request = require('axios') // a promise based HTTP client for the browser and Node.js.
-
-
-
-
-
-
-// array of camper objects
-const data = [
-{
-username: "SkyC0der",
-img: "https://avatars1.githubusercontent.com/u/24684319?v=4",
-alltime: 2323,
-recent: 153,
-lastUpdate: "2017-09-12T05:34:27.998Z"
-},
-{
-username: "sjames1958gm",
-img: "https://avatars1.githubusercontent.com/u/4639625?v=4",
-alltime: 8123,
-recent: 152,
-lastUpdate: "2017-09-12T05:34:41.025Z"
-},
-{
-username: "Manish-Giri",
-img: "https://avatars1.githubusercontent.com/u/11348778?v=3",
-alltime: 6160,
-recent: 130,
-lastUpdate: "2017-09-04T01:57:39.345Z"
-},
-{
-username: "heroiczero",
-img: "https://avatars.githubusercontent.com/u/21147717?v=3",
-alltime: 1374,
-recent: 122,
-lastUpdate: "2017-08-28T11:57:28.960Z"
-}
-];
-
-
-
-
-
+// const request = require('axios') // a promise based HTTP client for the browser and Node.js.
+import axios from 'axios';
 
 
 class CamperTable extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      campers: data
+      campers: [],
+      sortRecent: true
     };
   }
-
-
-
   componentDidMount() {
-
+    this.ajaxRequest();
   }
 
-  sortUsers = (event) => {
-    alert('hi');
+  ajaxRequest = () => {
+    const url = this.state.sortRecent ?
+                'https://fcctop100.herokuapp.com/api/fccusers/top/recent':
+                'https://fcctop100.herokuapp.com/api/fccusers/top/alltime';
+    const self = this;
+    axios.get(url)
+      .then(function(response) {
+        console.log(response);
+        self.setState({
+          campers: response.data
+        });
+      })
+      .catch(function(error) {
+        console.log(error);
+        alert('Whoops! Try reloading the page.')
+      });
+  }
+
+  sortUsers = (e) => {
+    const bool = e.target.id === "recent" ? true : false;
+    this.setState({
+      sortRecent: bool
+    }, this.ajaxRequest);
   }
 
   render() {
@@ -85,12 +63,12 @@ class CamperTable extends Component {
 const Header = (props) => {
   return (
     <thead>
-      <tr className="header row">
-        <th className="tab col-md-1 col-sm-1">{ "#" }</th>
-        <th className="tab col-md-5 col-sm-5" >{ "Camper" }</th>
-        <th className="tab pointsHd col-md-3 col-sm-3"
+      <tr className="row">
+        <th className=" col-md-1 col-sm-1">{ "#" }</th>
+        <th className=" col-md-5 col-sm-5" >{ "Camper" }</th>
+        <th className=" pointsHd col-md-3 col-sm-3" id="recent"
           onClick={props.sortUsers}>{ "Past 30 days" }</th>
-        <th className="tab pointsHd col-md-3 col-sm-3"
+        <th className=" pointsHd col-md-3 col-sm-3" id="alltime"
           onClick={props.sortUsers}>{ "All time" }</th>
         </tr>
     </thead>
@@ -101,18 +79,18 @@ const CamperRow = (props) => {
   return (
     <tr className="header row">
 
-      <td className="tab col-md-1 col-sm-1">{ props.rank }</td>
+      <td className=" col-md-1 col-sm-1">{ props.rank }</td>
 
-      <td className="tab col-md-5 col-sm-2">
+      <td className=" col-md-5 col-sm-2">
         <img src={ props.camper.img } alt="logo"></img>
         <p className="user">{ props.camper.username }</p>
       </td>
 
-      <td className="tab points col-md-3 col-sm-3" >
+      <td className=" points col-md-3 col-sm-3" >
         { props.camper.recent }
       </td>
 
-      <td className="tab points col-md-3 col-sm-3">
+      <td className=" points col-md-3 col-sm-3">
         { props.camper.alltime }
       </td>
 
